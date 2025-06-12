32252b5a51b6e488e50a321673ef80ced556f868
Andrei-Alexandrescu <163278111+Andrei-Alexandrescu@users.noreply.github.com>
2025-06-12 11:17:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/universe-curiosity-lab/',
   server: {
     host: "::",
     port: 8080,
